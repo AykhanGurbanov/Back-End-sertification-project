79d6a7b57ad1f8fe02e41d11a357201915700984
Aykhan <138309518+AykhanGurbanov@users.noreply.github.com>
2024-09-23 19:21:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,115 @@
-// index.js
-// where your node app starts
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+let mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
-// init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var unix = require('unix-timestamp')
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-app.use(bodyParser.urlencoded({extends: false}));
-app.use(bodyParser.json());
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+const UserSchema = new Schema({
+  username: String,
+})
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+const User = mongoose.model("User", UserSchema);
+
+const ExercisesSchema = new Schema({
+  user_id: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: { type: Date, required: true }
+})
+
+const Exercise = mongoose.model("Exercise", ExercisesSchema);
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-app.get("/api", (req, res, next) => {
-  req.timestamp = unix.now()
-  next();
-},(req, res,next)=>{
-  req.timestam = new Date().toUTCString();
-  next();
-},(req,res)=>{
+app.post('/api/users', async (req, res)=>{
+  const userName = new User ({
+    username: req.body.username
+  })
+  const user = await userName.save()
   res.json({
-    "unix": req.timestamp*1000,
-    "utc": req.timestam
+    "username":user.username,
+    "_id": user["_id"],
   })
 });
 
-app.get("/api/:number",(req, res) => {
-  const { number }  = req.params;
-  try{
-    if (number >= 13){
-      req.timestam = new Date(parseInt(number)).toUTCString();
-      if(req.timestam == "Invalid Date"){
-        res.json({error : "Invalid Date"})
-      }
-      res.json({
-      "unix": parseInt(number),
-      "utc": req.timestam
-    })
-    }else{
-      req.timestamp = unix.fromDate(number)
-      req.timestam = new Date(req.timestamp * 1000).toUTCString();
-      if(req.timestam == "Invalid Date"){
-        res.json({error : "Invalid Date"})
-      }
-      res.json({
-        "unix": req.timestamp*1000,
-        "utc": req.timestam
-      })
+app.post('/api/users/:_id/exercises',async (req, res)=>{
+  const id = req.params._id;
+  const userId = await User.findById(id)
+  if ( !userId ){
+    return res.json({"error": "User id is not found"})
+  } 
+  let date = req.body.date ? new Date(req.body.date) : new Date();
+  if (isNaN(date)) {
+    return res.json({ error: "Invalid date" });
+  }
+  const exercise = new Exercise({
+    user_id: id,
+    description: req.body.description,
+    duration: req.body.duration,
+    date: date
+  })
+  const exerci = await exercise.save()
+  res.json({
+    "_id": exerci["user_id"],
+    "username":userId.username,
+    "date": exerci.date.toDateString(),
+    "duration":exerci.duration,
+    "description":exerci.description
+  })
+})
+
+
+app.get('/api/users', async (req,res)=>{
+  const users = await User.find({}).select("_id username")
+  res.json(users)
+})
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  try {
+    const id = req.params._id;
+    const { from, to, limit } = req.query;
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    let query = { userId: id };
+    
+    if (from || to) {
+      query.date = {};
+      if (from) query.date.$gte = new Date(from);
+      if (to) query.date.$lte = new Date(to);
     }
-  }catch(err) {
-    if( err == "Invalid Date" ){
-      res.json({error : "Invalid Date"})
-    }
-  }  
+
+    let exercises = await Exercise.find({user_id:id});
+    console.log(exercises)
+    if (limit) exercises = exercises.slice(0, parseInt(limit));
+    const log = exercises.map(exercise => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: new Date(exercise.date).toDateString()
+    }));
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      count: log.length,
+      log: log
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
+
